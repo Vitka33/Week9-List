5882fda0397b4c3b332644c9ff03561a5025c3e3
Viktor Maljuga <vmaljuga@tktk.ee>
2022-03-05 12:35:06 +0200

    add new ptojects

---
+++ MyShopingList/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MyShopingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = @"C:\Users\...\samples\shopinglist";
+            string fileName = @"\\myShopingList.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myShopingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add something to shopinglist? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter what you like:");
+                    string userWish = Console.ReadLine();
+                    myShopingList.Add(userWish);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Take care!");
+                }
+
+            }
+            Console.Clear();
+
+            foreach (string thing in myShopingList)
+            {
+                Console.WriteLine($"Your things: {thing}");
+            }
+            File.WriteAllLines($"{fileLocation}{fileName}", myShopingList);
+        }
+    }
+}
---
+++ ShopingList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ShopingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //shopinglist
+            //myShopingList.txt
+            string rootDirectory = @"C:\Users\malju\samples";
+            Console.WriteLine("Enter directory name:");
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Enter file name:");
+            string fileName = Console.ReadLine();
+
+            if (Directory.Exists($"{rootDirectory}\\{newDirectory}") && File.Exists($"{rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory and File exists.");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+            }
+        }
+    }
+}
